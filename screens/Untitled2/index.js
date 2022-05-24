9e4333f37cc55f5317978ffa70873f4e6993adda
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-05-24 20:49:22 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,13 +1,17 @@
+import { TextInput } from "react-native";
 import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
 const Untitled2 = () => {
   return <View style={{
-    backgroundColor: '#f0f0f1',
-    padding: 10,
-    position: 'relative',
-    height: '100%'
+    left: 50,
+    top: 174,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
   }}>
       <Text style={{
       left: 39,
@@ -18,7 +22,15 @@ const Untitled2 = () => {
       lineHeight: 14,
       fontSize: 14,
       borderRadius: 0
-    }}>Loremtesting m… </Text></View>;
+    }}>Loremtesting m… </Text><TextInput></TextInput><TextInput style={{
+      left: 9,
+      top: 173,
+      position: "absolute",
+      backgroundColor: "#ffffff",
+      borderColor: "#cccccc",
+      width: 150,
+      height: 30
+    }} editable={false} placeholder="Tesing " defaultValue="Testing world tttt"></TextInput></View>;
 };
 
 export default Untitled2;
